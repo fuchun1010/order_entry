5ccb5f2ff4a1f756c837bf26311c37f676ffd8eb
fuchun198435163.com <tank198435@163.com>
2019-11-06 09:00:08 +0800

    [fuchun] #refactor remove unused code

--- src/main/java/com/tk/app/App.java
+++ src/main/java/com/tk/app/App.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 /**
+ * com.tk.app.mapper扫描更多的包，保持持久化接口存放位置灵活性
+ *
  * @author tank198435163.com
  * 订单域入口
  */
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
-@MapperScan("com.tk.app.mapper")
+@MapperScan("com.tk.app")
 public class App {
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
--- src/main/java/com/tk/app/mapper/Address.java
+++ src/main/java/com/tk/app/mapper/Address.java
@@ -4,6 +4,5 @@ package com.tk.app.mapper;
  * @author tank198435163.com
  */
 public interface Address {
-
   int find();
 }
